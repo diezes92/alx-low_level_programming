6915a910d93fcff8011e86ad50a5ba95d32da796
diezes92 <didierdiekpa@yahoo.fr>
2023-02-17 15:30:27 -0800

    task6vif

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+ch = '0';
+while (ch <= 9)
+{
+putchar(ch);
+ch++;
+}
+putchar('\n');
+return (0);
+}
