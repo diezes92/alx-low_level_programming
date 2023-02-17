724af06a981b0481ca9b81d69f6002a4ed8e7a25
diezes92 <didierdiekpa@yahoo.fr>
2023-02-17 14:11:08 -0800

    task3vif

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int c = 97;
+int i = 1;
+while (i < 27)
+{
+putchar(c);
+c++;
+i++;
+}
+c = 65;
+i = 1;
+while (i < 27)
+{
+putchar(c);
+c++;
+i++;
+}
+putchar('\n');
+return (0);
+}
