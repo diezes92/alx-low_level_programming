def5aa982e2e08b58baba1da81fcb374345daf75
diezes92 <didierdiekpa@yahoo.fr>
2023-02-17 14:27:58 -0800

    task4vif

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int c = 97;
+int i = 1;
+while (i < 27)
+{
+if (c == 101 || c == 113)
+{}
+else
+{
+putchar(c);
+}
+c++;
+i++;
+}
+putchar('\n');
+return (0);
+}
