78c7b2935d9f6649b12d8bca319fa7259b54c771
diezes92 <didierdiekpa@yahoo.fr>
2023-02-21 15:36:25 -0800

    task2fn

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,13 +1,20 @@
 #include "main.h"
+
 /**
- * print_alphabet_x10 - Entry point
+ * print_alphabet_x10 - Prints the alphabet 10 times.
  *
  * Return: Always 0 (Success)
  */
-void print_alphabet_x10(void) {
-int i;
-for (i = 1; i <= 10; i++)
+void print_alphabet_x10(void)
 {
-print_alphabet();
-}
+	int a, b;
+
+	for (a = 0; a <= 9; a++)
+	{
+		for (b = 97; b <= 122; b++)
+		{
+			_putchar(b);
+		}
+	_putchar('\n');
+	}
 }
