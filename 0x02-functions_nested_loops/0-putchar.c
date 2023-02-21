1a06375c175f5c8b151b53b607874378515f551c
diezes92 <didierdiekpa@yahoo.fr>
2023-02-21 14:14:47 -0800

    task0fn

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int tab[] = {95, 112, 117, 116, 99, 104, 97, 114};
+int i;
+for (i = 0; i <= 7; i++)
+{
+_putchar(tab[i]);
+}
+_putchar('\n');
+return (0);
+}
