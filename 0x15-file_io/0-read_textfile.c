69822c2eb36ceb32b1aeb3d9cda5d0f13a7829fe
diezes92 <didierdiekpa@yahoo.fr>
2023-04-09 05:54:51 -0700

    task0

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * read_textfile - reads a text file and prints the letters
+ * @filename: filename.
+ * @letters: numbers of letters printed.
+ *
+ * Return: numbers of letters printed. It fails, returns 0.
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+int fd;
+ssize_t rd, wr;
+char *mem;
+
+if (filename == NULL)
+return (0);
+fd = open(filename, O_RDONLY);
+if (fd == -1)
+return (0);
+mem = malloc(sizeof(char) * letters);
+if (mem == NULL)
+return (0);
+rd = read(fd, mem, letters);
+wr = write(STDOUT_FILENO, rd);
+return (wr);
+}
---
+++ 0x15-file_io/main.h
@@ -0,0 +1,12 @@
+#ifndef "main.h"
+#define "main.h"
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+ssize_t read_textfile(const char *filename, size_t letters);
+
+#endif
