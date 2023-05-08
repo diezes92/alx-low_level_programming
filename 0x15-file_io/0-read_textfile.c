10417f231e849cc747bb74d4ba2eeca5e48b0c7f
diezes92 <didierdiekpa@yahoo.fr>
2023-05-08 13:06:11 -0700

    tasks 0x15

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+ * read_textfile - reads a text file and prints the letters
+ * @filename: filename.
+ * @letters: numbers of letters printed.
+ *
+ * Return: numbers of letters printed. It fails, returns 0.
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+int op;
+ssize_t r, w;
+char *tmp;
+
+if (filename == NULL)
+return (0);
+
+op = open(filename, O_RDONLY);
+if (fd == -1)
+return (0);
+tmp = malloc(sizeof(char) * (letters));
+if (tmp == NULL)
+return (0);
+
+r = read(op, tmp, letters);
+w = write(STDOUT_FILENO, tmp, r);
+
+close(op);
+free(tmp);
+return (w);
+
+}
---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * create_file - creates a file
+ * @filename: filename.
+ * @text_content: content writed in the file.
+ *
+ * Return: 1 if it success. -1 if it fails.
+ */
+int create_file(const char *filename, char *text_content)
+{
+int op;
+int nb;
+int w;
+
+if (filename == NULL)
+return (-1);
+op = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+if (op == -1)
+return (-1);
+if (text_content == NULL)
+text_content = "";
+for (nb = 0; text_content[nb]; nb++)
+;
+w = write(op, text_content, nb);
+if (w == -1)
+return (-1);
+
+close(fd);
+return (1);
+
+}
